6e5964bc7bd0f30242ea44adb25c63ebe1fe9ab7
donbentley <donbentley@uga.edu>
2024-04-08 15:49:12 -0400

    add meter

--- career_raid/src/App.js
+++ career_raid/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Taskbar from './Taskbar';
 import SignUpPage from './SignUpPage';
+import RaidMeter from './RaidMeter';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Taskbar />
-        <SignUpPage/>
+        <RaidMeter />
       </div>
     </Router>
   );
